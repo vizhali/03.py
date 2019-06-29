d3bcbda98b2a55e28a54ed1de7bdb95b35e49112
vizhali <52244020+vizhali@users.noreply.github.com>
2019-06-29 12:02:41 +0530

    Update and rename README.md to 03.py

---
+++ 03.py
@@ -0,0 +1,7 @@
+n=input("")
+if n=='a'or n=='e'or n=='i'or n=='o'or n=='u'or n=='A'or n=='E'or n=='I'or n=='O'or n=='U':
+   print("Vowel")
+elif n=='z'or n=='b'or n=='c'or n=='f'or n=='g'or n=='Z'or n=='B'or n=='C'or n=='F'or n=='G':   
+   print("Consonant")
+else:
+   print("invalid")
